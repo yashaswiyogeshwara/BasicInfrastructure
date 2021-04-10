ad8052ce6bd9e13bfc6fdb5b1ddb853c4e07ffc7
yogey <yashaswi.yogeshwara@campsystems.com>
2021-04-10 15:56:41 +0530

    first commit

---
+++ BasicInfrastructure/BasicInfrastructure/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BasicInfrastructure.MvcApplication" Language="C#" %>
---
+++ BasicInfrastructure/BasicInfrastructure/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BasicInfrastructure.Startup))]
+namespace BasicInfrastructure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
